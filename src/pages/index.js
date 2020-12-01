404f89e72b09ebee08ec6a80dfaa25ee5bb48646
ANF-Studios <theanfstudios@gmail.com>
2020-12-02 03:20:55 +0500

    Added html tag and section

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,24 +7,29 @@ import Banner from "../components/banner"
 import "../styles/index.scss"
 
 const IndexPage = () => (
-  <div>
-    <meta charset="utf-8" />
-    <meta name="theme-color" content="#f2f4f5" />
-    <meta name="og:title" content="Black Frog Studios" />
-    <meta
-      name="twitter:description"
-      content="Black Frog Studios is a small video game development team founded in 2019. We have developed hits such as the Franklin franchise and Super Aleksandr Jones and the Round World. We focus on making titles with stunning stories and gameplay, as well as game assets and other media."
-    />
-    <meta
-      name="og:image"
-      content="https://github.com/ANF-Studios/Black-Frog2.github.io/blob/main/src/images/bfs-logo.png"
-    />
-    <Helmet>
-      <title>Home | Black Frog Studio</title>
-    </Helmet>
-    <Header />
-    <Banner />
-  </div>
+  <html lang="en">
+    {/*This is a comment btw*/}
+    <section>
+      <div>
+        <meta charset="utf-8" />
+        <meta name="theme-color" content="#f2f4f5" />
+        <meta name="og:title" content="Black Frog Studios" />
+        <meta
+          name="twitter:description"
+          content="Black Frog Studios is a small video game development team founded in 2019. We have developed hits such as the Franklin franchise and Super Aleksandr Jones and the Round World. We focus on making titles with stunning stories and gameplay, as well as game assets and other media."
+        />
+        <meta
+          name="og:image"
+          content="https://github.com/ANF-Studios/Black-Frog2.github.io/blob/main/src/images/bfs-logo.png"
+        />
+        <Helmet>
+          <title>Home | Black Frog Studio</title>
+        </Helmet>
+        <Header />
+        <Banner />
+      </div>
+    </section>
+  </html>
 )
 
 export default IndexPage
