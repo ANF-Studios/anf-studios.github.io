cbf47aedd7d3b30ff79c45c0ba21d67c948959e1
ANF-Studios <theanfstudios@gmail.com>
2020-12-03 02:04:26 +0500

    Updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,31 +6,35 @@ import Header from "../components/header"
 import Banner from "../components/banner"
 import "../styles/index.scss"
 
-const IndexPage = () => (
-  <html lang="en">
-    {/*This is a comment btw*/}
-    <section>
-      <div>
-        <meta charset="utf-8" />
-        <meta name="theme-color" content="#f2f4f5" />
-        <meta name="og:title" content="Black Frog Studios" />
-        <meta
-          name="twitter:description"
-          content="Black Frog Studios is a small video game development team founded in 2019. We have developed hits such as the Franklin franchise and Super Aleksandr Jones and the Round World. We focus on making titles with stunning stories and gameplay, as well as game assets and other media."
-        />
-        <meta
-          name="og:image"
-          content="https://github.com/ANF-Studios/Black-Frog2.github.io/blob/main/src/images/bfs-logo.png"
-        />
-        <Helmet>
-          <title>Home | Black Frog Studio</title>
-        </Helmet>
-        <Header />
-        <Banner />
-      </div>
-    </section>
-  </html>
-)
+const IndexPage = () => {
+  return (
+    <html lang="en">
+      {/*This is a comment btw*/}
+      <section>
+        <div>
+          <head>
+            <meta charset="utf-8" />
+            <meta name="theme-color" content="#f2f4f5" />
+            <meta name="og:title" content="Black Frog Studios" />
+            <meta
+              name="twitter:description"
+              content="Black Frog Studios is a small video game development team founded in 2019. We have developed hits such as the Franklin franchise and Super Aleksandr Jones and the Round World. We focus on making titles with stunning stories and gameplay, as well as game assets and other media."
+            />
+            <meta
+              name="og:image"
+              content="https://github.com/ANF-Studios/Black-Frog2.github.io/blob/main/src/images/bfs-logo.png"
+            />
+            <Helmet>
+              <title>Home | Black Frog Studio</title>
+            </Helmet>
+          </head>
+          <Header />
+          <Banner />
+        </div>
+      </section>
+    </html>
+  )
+}
 
 export default IndexPage
 // https://github.com/wrongakram/odunsi-starter
