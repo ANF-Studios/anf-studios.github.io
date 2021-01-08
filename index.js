a55894fd460322924e6f9c8fda834b051e9a6a46
ANF-Studios <theanfstudios@gmail.com>
2021-01-08 13:28:46 -0500

    Fixed navbar display issue

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 let navbar = document.getElementById("navbar");
 let sticky = navbar.offsetTop;
 
-console.log(navbar == null || undefined);
-
 // This part is responsible for the sticky navbar.
 window.onscroll = function () {
   if (window.pageYOffset >= sticky) navbar.classList.add("sticky");
-  else navbar.classList.remove("sticky");
 };
