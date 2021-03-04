6af6882f196ba6f301e715d09c0f8611bf2ff635
ANF-Studios <theanfstudios@gmail.com>
2021-03-04 11:20:35 -0500

    Removed redundant code

--- index.js
+++
@@ -1,7 +0,0 @@
-let navbar = document.getElementById("navbar");
-let sticky = navbar.offsetTop;
-
-// This part is responsible for the sticky navbar.
-window.onscroll = function () {
-  if (window.pageYOffset >= sticky) navbar.classList.add("sticky");
-};
